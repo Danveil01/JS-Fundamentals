5677ea775cc8316b5618f02e1ec4a2759de461a0
Ampaabeng Kyeremeh Nancy <kampaabeng01@gmail.com>
2025-11-09 14:07:52 -0800

    10-factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,10 @@
+function factorial(n) {
+  n = parseInt(n, 10);
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = process.argv[2];
+console.log(factorial(num));
